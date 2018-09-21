c25dee9594b1f9ce15c4f66816a0889f4df0d31b
Michal Sitarczuk <michalsk@axis.com>
2018-09-21 16:28:51 +0200

    Continuation on the assessment.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # A_C_S_mdh.se
+
+mdh.se
+Applied Cyber Security course
+author: Michal Sitarczuk
+A.D. 2018
--- httpd.js
+++ httpd.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var path = require("path");
 var port = process.argv[2] || 8000;
+var files = [];
 
 port = parseInt(port, 10);
 
@@ -12,37 +13,63 @@ var server = http.createServer((req, res) => {
 
     try{
         var uri = url.parse(req.url)
-        var fsPath = req.pathname
         
-        var fileStream = fs.createReadStream(fsPath)
-        fileStream.pipe(res)
-        fileStream.on('open', function)
+
+        var contentTypesByExtensions = {
+            '.html' : "text/html",
+            '.css' : "text/css",
+            '.js' : "javascript"
+        };
+            // if(fs.statSync(fsPath).isDirectory()){
+            //     fsPath += '/index.html';
+            // }
+            var fsPath = uri.pathname.substring(1)
+            var fileStream = fs.createReadStream(fsPath)
+            fileStream.pipe(res)
+            fileStream.on('open', function() {
+            res.writeHead(200)
+            })
+            fileStream.on('error', function(){
+                fs.readdir(fsPath.path, function(err,list){
+                    if(err) throw err;
+                    for(var i=0; i<list.length; i++)
+                    {
+                        path.extname(list[i])
+                        console.log(list[i]); //print the file
+                        files.push(list[i]); //store the file name into the array files
+                    }
+                });
+            })
+    } catch(e) {
+        res.writeHead(404)
+        res.end("404 Error occured when reading file")
+        console.log(e.stack)
     }
 
-    fs.exists(filename, function(exists) {
-        if(!exists) {
-            res.writeHead(404, {'Content-Type' : 'text/plain'});
-            res.write("404 Not found\n");
-            res.end();
-            return;
-        }
+    // fs.exists(filename, function(exists) {
+    //     if(!exists) {
+    //         res.writeHead(404, {'Content-Type' : 'text/plain'});
+    //         res.write("404 Not found\n");
+    //         res.end();
+    //         return;
+    //     }
 
-        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    //     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-        fs.readFile(filename, "binary", function(err, file){
-            if(err) {
-                res.writeHead(500, {"Content-Type" : "text/plain"});
-                res.write(err + "\n");
-                res.end();
-                return;
-            }
+    //     fs.readFile(filename, "binary", function(err, file){
+    //         if(err) {
+    //             res.writeHead(500, {"Content-Type" : "text/plain"});
+    //             res.write(err + "\n");
+    //             res.end();
+    //             return;
+    //         }
 
-            res.writeHead(200, { 'Content-Type': 'text/html'});
-            //res.write("<h1>Hello World</h1>");
-            res.write(file, "binary");
-            res.end();
-        });
-    });
+    //         res.writeHead(200, { 'Content-Type': 'text/html'});
+    //         //res.write("<h1>Hello World</h1>");
+    //         res.write(file, "binary");
+    //         res.end();
+    //     });
+    // });
 }).listen(port);
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
