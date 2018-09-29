89308a1aa3e34f91a19b7bb7ed8f8e3f822db10e
Michal Sitarczuk <[]\/[]oaiddip@gmail.com>
2018-09-29 18:04:01 +0200

    Lab1 done, ver. 0.1

--- httpd.js
+++ httpd.js
@@ -46,71 +46,69 @@ http.createServer((req, res) => {
         body = [];
         });
 
-        fs.exists(pathname, function (exist) {
-            if(!exist) {
-              res.statusCode = 404;
-              res.end(`File ${pathname} not found!`);
-              return;
-            }
-            if (fs.statSync(pathname).isDirectory()){
-                pathname += '/index.html';
-            }
+        if(pathname==="./information"){
+            fs.readFile("./templates/information.html", function(err, data){
+                if(data){
+                    var informationHtmlString = data.toString("utf8");
+                    informationHtmlString = informationHtmlString.replace("{{method}}", req.method);
+                    informationHtmlString = informationHtmlString.replace("{{path}}", uri.path.substring(0, uri.path.indexOf("?")));
+                    //informationHtmlString = informationHtmlString.replace("{{query}}", JSON.stringify(uri.query));
+                    informationHtmlString = informationHtmlString.replace("{{query}}", uri.path.substring(uri.path.indexOf("?")));
 
-            fs.readFile(pathname, function(err, data){
-                if(err){
-                //   res.statusCode = 500;
-                //   res.end(`Error getting the file: ${err}.`);
-                pathname = pathname.slice (0, -11);
-                console.log(pathname);
-                fs.readdir(pathname, function(err,list){
-                            if(err) throw err;
-                            //files = [];
-                            for(var i=0; i<list.length; i++)
-                            {
-                                path.extname(list[i])
-                                //console.log(list[i]); //print the file
-                                files.push(list[i]);
-                                files.sort();
-                            }
-                                res.writeHead(200, 'Content-type', 'text/plain');
-                                res.end(files.join('\n'));
-                        });
-                } else {
-                  res.writeHead(200, 'Content-type', contentTypesByExtensions[ext] || 'text/plain');
-                  res.end(data);
+                    var parsedQueries = "<ul>";
+                    Object.entries(uri.query).forEach((element, key) => {
+                        parsedQueries += "<li>"+element[0]+" : "+element[1]+"</li>"
+                    });
+                    parsedQueries += "</ul>"
+                    informationHtmlString = informationHtmlString.replace("{{queries}}", parsedQueries);
+
+                    res.statusCode = 200;
+                    res.end(informationHtmlString);
+                    return;
                 }
             });
-        });
+        }else{
+            fs.exists(pathname, function (exist) {
+                if(!exist) {
+                res.statusCode = 404;
+                res.end(`File ${pathname} not found!`);
+                return;
+                }
+                if (fs.statSync(pathname).isDirectory()){
+                    pathname += '/index.html';
+                }
+
+                fs.readFile(pathname, function(err, data){
+                    if(err){
+                    //   res.statusCode = 500;
+                    //   res.end(`Error getting the file: ${err}.`);
+                    pathname = pathname.slice (0, -11);
+                    console.log(pathname);
+                    fs.readdir(pathname, function(err,list){
+                                if(err) throw err;
+                                //files = [];
+                                for(var i=0; i<list.length; i++)
+                                {
+                                    path.extname(list[i])
+                                    //console.log(list[i]); //print the file
+                                    files.push(list[i]);
+                                    files.sort();
+                                }
+                                    res.writeHead(200, 'Content-type', 'text/plain');
+                                    res.end(files.join('\n'));
+                            });
+                    } else {
+                    res.writeHead(200, 'Content-type', contentTypesByExtensions[ext] || 'text/plain');
+                    res.end(data);
+                    }
+                });
+            });
+        }
     } catch(e) {
         res.writeHead(404)
         res.end("404 Error occured when reading file")
         console.log(e.stack)
     }
-
-    // fs.exists(filename, function(exists) {
-    //     if(!exists) {
-    //         res.writeHead(404, {'Content-Type' : 'text/plain'});
-    //         res.write("404 Not found\n");
-    //         res.end();
-    //         return;
-    //     }
-
-    //     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    //     fs.readFile(filename, "binary", function(err, file){
-    //         if(err) {
-    //             res.writeHead(500, {"Content-Type" : "text/plain"});
-    //             res.write(err + "\n");
-    //             res.end();
-    //             return;
-    //         }
-
-    //         res.writeHead(200, { 'Content-Type': 'text/html'});
-    //         //res.write("<h1>Hello World</h1>");
-    //         res.write(file, "binary");
-    //         res.end();
-    //     });
-    // });
 }).listen(port);
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
